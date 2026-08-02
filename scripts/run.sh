47062e87e291e4e966254fee2666a44482b78b12
Jerry <sgysun36@liverpool.ac.uk>
2026-08-02 11:06:26 +0800

    Configure full GRPO training run

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,17 +3,17 @@ uv run --no-sync python -m scripts.grpo_train \
     --prompt-path cs336_alignment/prompts/r1_zero.prompt \
     --train-path data/gsm8k/train.jsonl \
     --validation-path data/gsm8k/test.jsonl \
-    --num-rollout-steps 1 \
-    --rollout-batch-size 8 \
-    --group-size 2 \
-    --gradient-accumulation-steps 2 \
+    --num-rollout-steps 50 \
+    --rollout-batch-size 256 \
+    --group-size 8 \
+    --gradient-accumulation-steps 32 \
     --learning-rate 1e-5 \
     --max-grad-norm 1.0 \
     --temperature 1.0 \
-    --max-tokens 128 \
+    --max-tokens 512 \
     --eval-every 10 \
     --log-rollout-every 40 \
-    --n-validation-samples 32 \
+    --n-validation-samples 1024 \
     --seed 0 \
     --training-device cuda:0 \
     --vllm-gpu 1
